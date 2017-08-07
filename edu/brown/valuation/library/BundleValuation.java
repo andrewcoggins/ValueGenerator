4b94a0eb57bc4a62249d7a13776fc02acbcc938b
Andrew Coggins <acoggins@cs.brown.edu>
2017-08-07 16:30:51 -0400

    implement generators

--- edu/brown/valuation/library/BundleValuation.java
+++ edu/brown/valuation/library/BundleValuation.java
@@ -2,9 +2,11 @@ package brown.valuation.library;
 
 import java.util.AbstractMap.SimpleEntry;
 import java.util.Map.Entry;
+import java.util.Set;
 
 import brown.valuable.IValuable;
 import brown.valuable.library.Bundle;
+import brown.valuable.library.Good;
 import brown.valuation.IValuation;
 
 public class BundleValuation implements IValuation {
@@ -14,6 +16,7 @@ public class BundleValuation implements IValuation {
   public BundleValuation(Bundle aBundle, Double price) {
     this.entry = new SimpleEntry<Bundle, Double>(aBundle, price);
   }
+  
   @Override
   public IValuable getValuable() {
     return (Bundle) this.entry.getKey();
